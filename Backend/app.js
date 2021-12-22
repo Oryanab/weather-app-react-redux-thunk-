eeaf49af55eec10eda4f958cd704b755687ab0d2
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-12-22 23:20:54 +0200

    update 05

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("../build"));
+app.get("/", function (req, res) {
+  // serve main path as static file
+  res.sendFile(path.resolve("../build/index.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`app listening at http://localhost:${PORT}`)
+);
